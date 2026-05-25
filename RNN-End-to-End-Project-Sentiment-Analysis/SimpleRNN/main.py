d8ee58c143e1f64b803ed11edcf1ab2d88a26d30
tanisshka <tanishkapatil0102@gmail.com>
2026-05-25 23:06:12 +0530

    Update commit

--- RNN-End-to-End-Project-Sentiment-Analysis/SimpleRNN/main.py
+++ RNN-End-to-End-Project-Sentiment-Analysis/SimpleRNN/main.py
@@ -1,6 +1,5 @@
 #Step 1: Import Lib and load the model
 import numpy as np
-import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
